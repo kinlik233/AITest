acef93993f759637764852f35ca50bb0460eede5
kinlik <2841711990@qq.com>
2026-08-18 17:47:32 +0800

    add main.cs

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+namespace main
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("helloword");
+            Console.ReadKey();
+        }
+
+    }
+}
